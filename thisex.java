59c596b9407d93730a29e47a7b61d74f626f516a
VaghelaAnil7144 <anilvaghela2008@gmail.com>
2025-03-27 09:13:46 +0530

    Add files via upload

---
+++ thisex.java
@@ -0,0 +1,21 @@
+class thisex
+	{
+		int rollno;
+		String name;
+		public thisex(int rollno,String name)
+		{
+			this.rollno=rollno;
+			this.name=name;
+		}
+		void display()
+		{
+			System.out.println(rollno);
+			System.out.println(name);
+
+		}
+		public static void main(String args[])
+		{
+			thisex s1=new thisex(7125,"nnn");
+			
+		}
+	}
